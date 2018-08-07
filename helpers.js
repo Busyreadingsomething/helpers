01bcc2c5a221bd00d5270cf1275338acd415a0d6
Makan Azarshahy <oro1919@gmail.com>
2018-08-06 22:08:12 -0700

    Added MAP method

--- helpers.js
+++ helpers.js
@@ -1,17 +1,31 @@
-const X = {
+export default {
   all(obj, callback) {
     if (Array.isArray(obj)) {
       for(let i = 0; i < obj.length; i += 1) {
-        if (callback) callback(obj[i]);
+        if (callback) callback(obj[i], i, obj);
       }
     } else if (obj !== null && typeof obj === 'object') {
       for (const key in obj) {
-        if (callback) callback(obj[key]);
+        if (callback) callback(obj[key], key, obj);
       }
     } else {
-      throw 'Please submit an Array or Object';
+      throw new Error('Please submit an Array or Object');
     }
   },
+  map(obj, callback) {
+    let result;
+    if (Array.isArray(obj)) {
+      result = [];
+      this.all(obj, (item, index, obj) => {
+        result.push(callback(item, index, obj));
+      });
+    } else if (obj !== null && typeof obj === 'object') {
+      result = {};
+      this.all(obj, (item, key, obj) => {
+        result[key] = callback(item, key, obj);
+      });
+    } else {
+      throw new Error('Object or Array was not given');
+    }
+  }
 };
-
-export default X;
